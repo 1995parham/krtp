f86246d731a81c857af9cc9bcc39813eabb24acd
Parham Alvani <parham.alvani@gmail.com>
2017-06-02 16:55:40 +0430

    CSRC section was added.

--- rtp-packet.js
+++ rtp-packet.js
@@ -22,13 +22,21 @@ class RTPPacket {
     this.ssrc = ssrc;
     this.payloadType = payloadType;
     this.timestamp = timestamp;
+    this.csrc = [];
+  }
+
+  addCSRC(csrc) {
+    if (csrc.length > 31) {
+      return;
+    }
+    this.csrc.append(csrc);
   }
 
   serialize() {
-    const buff = Buffer.alloc(12 + this.payload.length);
+    const buff = Buffer.alloc(12 + 4 * this.csrc.length + this.payload.length);
 
     /* buff[0] = (V << 6 | P << 5 | X << 4 | CC) */
-    buff[0] = 0x80;
+    buff[0] = (2 << 6 | 0 << 5 | 0 << 4 | this.csrc.length);
     /* buff[1] = (M << 7 | PT) */
     buff[1] = (0 << 7 | this.payloadType);
     /* buff[2, 3] = SN */
@@ -38,10 +46,16 @@ class RTPPacket {
     /* buff[8, 9, 10, 11] = SSRC */
     buff.write(this.ssrc, 8, 4);
 
+    /* CSRC section */
+    for (let i = 0; i < this.csrc.length; i++) {
+      buff.write(this.csrc[i], 12 + i * 4, 4);
+    }
+
     this.payload.copy(buff, 12, 0);
 
     return buff;
   }
+
 }
 
 module.exports = RTPPacket;
