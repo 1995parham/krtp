4880b3096c07e3a6a4d2a3c4fe8009c052405f77
Parham Alvani <parham.alvani@gmail.com>
2017-06-02 17:23:11 +0430

    RTP packet deserialization was added.

--- rtp-packet.js
+++ rtp-packet.js
@@ -29,7 +29,7 @@ class RTPPacket {
     if (csrc.length > 31) {
       return;
     }
-    this.csrc.append(csrc);
+    this.csrc.push(csrc);
   }
 
   serialize() {
@@ -51,11 +51,45 @@ class RTPPacket {
       buff.write(this.csrc[i], 12 + i * 4, 4);
     }
 
-    this.payload.copy(buff, 12, 0);
+    this.payload.copy(buff, 12 + this.csrc.length * 4, 0);
 
     return buff;
   }
 
+  static deserialize(buff) {
+    const csrc = [];
+
+    /* buff[0] = (V << 6 | P << 5 | X << 4 | CC) */
+    if (buff[0] && 0xC0 != 2) {
+      return null;
+    }
+    const cc = buff[0] && 0x1F;
+    /* buff[1] = (M << 7 | PT) */
+    const payloadType = (buff[1] && 0x8F);
+    /* buff[2, 3] = SN */
+    const sequenceNumber = buff.readUInt16BE(2);
+    /* buff[4, 5, 6, 7] = TS */
+    const timestamp = buff.readUInt32BE(4);
+    /* buff[8, 9, 10, 11] = SSRC */
+    const ssrc = buff.slice(8, 12).toString();
+
+    /* CSRC section */
+    for (let i = 0; i < cc; i++) {
+      csrc.append(buff.slice(12 + i * 4, 12 + i * 4 + 4).toString());
+    }
+
+
+    const payload = Buffer.from(buff.slice(12 + csrc.length * 4));
+
+    const packet = new RTPPacket(payload, sequenceNumber, ssrc, timestamp,
+      payloadType);
+
+    for (let i = 0; i < csrc.length; i++) {
+      packet.addCSRC(csrc[i]);
+    }
+
+    return packet;
+  }
 }
 
 module.exports = RTPPacket;
