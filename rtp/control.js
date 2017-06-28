83324d89c1dc5c25a1d096cedc41ccafb516f678
Parham Alvani <parham.alvani@gmail.com>
2017-06-28 10:13:41 +0430

    Control.js reformatted.

--- rtp/control.js
+++ rtp/control.js
@@ -8,91 +8,88 @@
  * +===============================================
  */
 
-
 class RTPControlSR {
-  constructor(packetCount, octetCount, ssrc, timestamp, ntpTimestamp) {
-    this.packetCount = packetCount;
-    this.octetCount = octetCount;
-    this.timestamp = timestamp;
+  constructor (packetCount, octetCount, ssrc, timestamp, ntpTimestamp) {
+    this.packetCount = packetCount
+    this.octetCount = octetCount
+    this.timestamp = timestamp
     if (ntpTimestamp) {
-      this.ntpTimestamp = ntpTimestamp;
+      this.ntpTimestamp = ntpTimestamp
     } else {
-      this.ntpTimestamp = Date.now();
+      this.ntpTimestamp = Date.now()
     }
-    this.ssrc = ssrc;
-    this.rc = 0;
+    this.ssrc = ssrc
+    this.rc = 0
   }
 
-  serialize() {
-    const buff = Buffer.alloc(8 + 20);
+  serialize () {
+    const buff = Buffer.alloc(8 + 20)
 
     /* header */
 
     /* buff[0] = (V << 6 | P << 5 | RC) */
-    buff[0] = (2 << 6 | 0 << 5 | this.rc);
+    buff[0] = (2 << 6 | 0 << 5 | this.rc)
     /* buff[1] = PT */
-    buff[1] = 200;
+    buff[1] = 200
     /* buff[2, 3] = length */
-    buff.writeUInt16BE((buff.length / 4 | 0) - 1, 2);
+    buff.writeUInt16BE((buff.length / 4 | 0) - 1, 2)
     /* buff[4, 5, 6, 7] = SSRC */
-    buff.writeUInt32BE(this.ssrc, 4);
+    buff.writeUInt32BE(this.ssrc, 4)
 
     /* sender info */
 
     /* buff[8, 9, 10, 11] = ntpTS */
-    buff.writeUInt32BE(this.ntpTimestamp / 1000 | 0, 8);
+    buff.writeUInt32BE(this.ntpTimestamp / 1000 | 0, 8)
     /* buff[12, 13, 14, 15] = ntpTS */
-    buff.writeUInt32BE(this.ntpTimestamp % 1000, 12);
+    buff.writeUInt32BE(this.ntpTimestamp % 1000, 12)
     /* buff[16, 17, 18, 19] = TS */
-    buff.writeUInt32BE(this.timestamp, 16);
+    buff.writeUInt32BE(this.timestamp, 16)
     /* buff[20, 21, 22, 23] = packetCount */
-    buff.writeUInt32BE(this.packetCount, 20);
+    buff.writeUInt32BE(this.packetCount, 20)
     /* buff[24, 25, 26, 27] = octetCount */
-    buff.writeUInt32BE(this.octetCount, 24);
+    buff.writeUInt32BE(this.octetCount, 24)
 
-    return buff;
+    return buff
   }
 
-  static deserialize(buff) {
-
+  static deserialize (buff) {
     /* header */
 
     /* buff[0] = (V << 6 | P << 5 | RC) */
     if (buff[0] & 0xC0 >> 6 !== 2) {
-      return null;
+      return null
     }
     /* buff[1] = PT */
     if (buff[1] !== 200) {
-      return null;
+      return null
     }
     /* buff[2, 3] = length */
-    const length = (buff.readUInt16BE(2) + 1) * 4;
+    const length = (buff.readUInt16BE(2) + 1) * 4
     if (buff.length !== length) {
-      return null;
+      return null
     }
     /* buff[4, 5, 6, 7] = SSRC */
-    const ssrc = buff.readUInt32BE(4);
+    const ssrc = buff.readUInt32BE(4)
 
     /* sender info */
 
     /* buff[8, 9, 10, 11] = ntpTS */
     /* buff[12, 13, 14, 15] = ntpTS */
-    const ntpTimestamp = buff.readUInt32BE(8) * 1000 + buff.readUInt32BE(12);
+    const ntpTimestamp = buff.readUInt32BE(8) * 1000 + buff.readUInt32BE(12)
     /* buff[16, 17, 18, 19] = TS */
-    const timestamp = buff.readUInt32BE(16);
+    const timestamp = buff.readUInt32BE(16)
     /* buff[20, 21, 22, 23] = packetCount */
-    const packetCount = buff.readUInt32BE(20);
+    const packetCount = buff.readUInt32BE(20)
     /* buff[24, 25, 26, 27] = octetCount */
-    const octetCount = buff.readUInt32BE(24);
-
+    const octetCount = buff.readUInt32BE(24)
 
     const packet = new RTPControlSR(packetCount, octetCount, ssrc,
-      timestamp, ntpTimestamp);
+      timestamp, ntpTimestamp)
 
-    return packet;
+    return packet
   }
 }
 
 module.exports = {
   RTPControlSR
-};
+}
