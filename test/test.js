0fb6e95b5bb7e9243c8f775f60ad73368de97e21
Parham Alvani <parham.alvani@gmail.com>
2017-06-28 10:17:26 +0430

    Test.js reformatted.

--- test/test.js
+++ test/test.js
@@ -9,58 +9,56 @@
  */
 /* eslint-env mocha */
 
-const assert = require('assert');
+const assert = require('assert')
 
-const krtp = require('..');
+const krtp = require('..')
 
 describe('RTPSession', () => {
-
   it('packet send-recieve serialize-deserialize', (done) => {
-    const s = new krtp.RTPSession(1373);
+    const s = new krtp.RTPSession(1373)
     s.on('message', (msg) => {
-      assert.equal(s.sequenceNumber, msg.sequenceNumber + 1);
-      assert.equal(s.ssrc, msg.ssrc);
-      assert.equal('Hello world', msg.payload.toString());
-      s.close();
-      done();
-    });
-    s.send(Buffer.from('Hello world')).catch((err) =>{
-      done(err);
-    });
-  });
+      assert.equal(s.sequenceNumber, msg.sequenceNumber + 1)
+      assert.equal(s.ssrc, msg.ssrc)
+      assert.equal('Hello world', msg.payload.toString())
+      s.close()
+      done()
+    })
+    s.send(Buffer.from('Hello world')).catch((err) => {
+      done(err)
+    })
+  })
 
   it('packet send payload must be buffer', (done) => {
-    const s = new krtp.RTPSession(1373);
+    const s = new krtp.RTPSession(1373)
     s.send('Hello world')
       .then(() => {
-        s.close();
-        done(new Error('we have no exception ?!'));
+        s.close()
+        done(new Error('we have no exception ?!'))
       }).catch(() => {
-        s.close();
-        done();
-      });
-  });
+        s.close()
+        done()
+      })
+  })
 
   it('realtime streaming with 2 packet', (done) => {
-    const s = new krtp.RTPSession(1373);
-    let c = 0;
-    let sq = 0;
+    const s = new krtp.RTPSession(1373)
+    let c = 0
+    let sq = 0
     s.on('message', (msg) => {
       if (sq === 0) {
-        sq = msg.sequenceNumber;
+        sq = msg.sequenceNumber
       } else {
-        assert.equal(msg.sequenceNumber, sq + 1);
+        assert.equal(msg.sequenceNumber, sq + 1)
       }
-      assert.equal(msg.timestamp, c);
-      c++;
+      assert.equal(msg.timestamp, c)
+      c = c + 1
       if (c === 2) {
-        done();
+        done()
       }
-    });
-    const rws = krtp.createWriteStream(1, 1373);
-    rws.write(Buffer.from('Hello'));
-    rws.write(Buffer.from('World'));
-    rws.end();
-  });
-
-});
+    })
+    const rws = krtp.createWriteStream(1, 1373)
+    rws.write(Buffer.from('Hello'))
+    rws.write(Buffer.from('World'))
+    rws.end()
+  })
+})
