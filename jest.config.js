29645cb7d8f869583d0fddc1c7e25f6254412497
Parham Alvani <parham.alvani@gmail.com>
2019-01-02 13:16:12 +0330

    Adds jest configuration.

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 02-01-2019
+ * |
+ * | File Name:     jest.config.js
+ * +===============================================
+ */
+module.exports = {
+  "roots": [
+    "<rootDir>"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
