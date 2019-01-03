95fabb68817eea2fcedadd198d847ff6eff88350
Parham Alvani <parham.alvani@gmail.com>
2019-01-03 10:35:24 +0330

    Tests rxjs.

--- __tests__/test.spec.ts
+++ __tests__/test.spec.ts
@@ -10,21 +10,41 @@
 /* eslint-env mocha */
 
 import * as assert from 'assert';
+import { range } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 import { Session } from '..';
 
 describe('RTPSession', () => {
-  it('packet send-recieve serialize-deserialize', (done) => {
-    const s = new Session(1373)
+  test('packet send-recieve serialize-deserialize', done => {
+    const s = new Session(1373);
     s.on('message', (msg) => {
-      assert.equal(s.sequenceNumber, msg.sequenceNumber + 1)
-      assert.equal(s.ssrc, msg.ssrc)
-      assert.equal('Hello world', msg.payload.toString())
-      s.close()
-      done()
-    })
+      assert.equal(s.sequenceNumber, msg.sequenceNumber + 1);
+      assert.equal(s.ssrc, msg.ssrc);
+      assert.equal('Hello world', msg.payload.toString());
+      s.close();
+      done();
+    });
     s.send(Buffer.from('Hello world')).catch((err) => {
-      done(err)
-    })
+      done(err);
+    });
+  })
+
+  test('rxjs', done => {
+    const s = new Session(1372);
+    const initialSequenceNumber = s.sequenceNumber;
+
+    s.message$.pipe(
+      filter(msg => msg.sequenceNumber === initialSequenceNumber + 9)
+    ).subscribe((msg) => {
+      assert.equal('Hello world of rxjs - 10', msg.payload.toString());
+      done();
+    });
+
+    range(1, 10).subscribe((i: number) => {
+      s.send(Buffer.from(`Hello world of rxjs - ${i}`)).catch((err) => {
+        done(err)
+      })
+    });
   })
 })
