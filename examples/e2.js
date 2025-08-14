506318c8cb97fbbed5ba22a257c4ada139446be8
Parham Alvani <parham.alvani@gmail.com>
2025-08-14 06:51:52 +0000

    fix: remove js ignorance

---
+++ examples/e2.js
@@ -0,0 +1,9 @@
+import { Session, ReadRTPStream, WriteRTPStream } from "../index.js";
+const s = new Session(1372);
+const r = new ReadRTPStream(s);
+const w = new WriteRTPStream(s, "127.0.0.1");
+r.on("data", (chunk) => {
+    console.log(chunk.toString());
+});
+w.write("Hello Elahe");
+w.write("I am Parham");
